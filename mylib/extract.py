14ba9aa00aafda7ed1466fde62f3b183cfa3f45b
XueqingWu <47194238+XueqingWu@users.noreply.github.com>
2023-10-01 00:13:04 +0000

    change the yml file

--- mylib/extract.py
+++ mylib/extract.py
@@ -7,12 +7,12 @@ import requests
 import os
 
 def extract(url=
-            "https://github.com/fivethirtyeight/data/blob/master/births/US_births_2000-2014_SSA.csv?raw=true ", 
-             # Add ?raw=true so only run the csv not the html
-            file_path="data/US_births_2000-2014_SSA.csv", 
-            # when data come into "data" directory, save as US_births_2000-2014_SSA.csv
-            directory="data"):
-             # where to put the data when the github data come in
+    "https://github.com/fivethirtyeight/data/blob/master/births/US_births_2000-2014_SSA.csv?raw=true ", 
+    # Add ?raw=true so only run the csv not the html
+    file_path="data/US_births_2000-2014_SSA.csv", 
+    # when data come into "data" directory, save as US_births_2000-2014_SSA.csv
+     directory="data"):
+     # where to put the data when the github data come in
     """"Extract a url to a file path"""
 
 
