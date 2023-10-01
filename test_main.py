95a3273675564f6ac2ac8b1eee20587f6a7acc58
XueqingWu <47194238+XueqingWu@users.noreply.github.com>
2023-10-01 00:27:26 +0000

    update test

--- test_main.py
+++ test_main.py
@@ -3,8 +3,117 @@ Test goes here
 
 """
 
-from mylib.calculator import add
+import subprocess
 
 
-def test_add():
-    assert add(1, 2) == 3
+def test_extract():
+    """tests extract()"""
+    result = subprocess.run(
+        ["python", "main.py", "extract"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+    assert "Extracting data..." in result.stdout
+
+
+def test_transform_load():
+    """tests transfrom_load"""
+    result = subprocess.run(
+        ["python", "main.py", "transform_load"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+    assert "Transforming data..." in result.stdout
+
+
+def test_update_record():
+    """tests update_record"""
+    result = subprocess.run(
+        [
+            "python",
+            "main.py",
+            "update_record",
+            "1",
+            "2000",
+            "1",
+            "1",
+            "6",
+            "9083",
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+
+
+def test_delete_record():
+    """tests delete_record"""
+    result = subprocess.run(
+        ["python", "main.py", "delete_record", "1"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+
+
+def test_create_record():
+    """tests create_record"""
+    result = subprocess.run(
+        [
+            "python",
+            "main.py",
+            "create_record",
+            "2000",
+            "1",
+            "1",
+            "6",
+            "9083",
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+
+
+def test_general_query():
+    """tests general_query"""
+    result = subprocess.run(
+        [
+            "python",
+            "main.py",
+            "general_query",
+            "SELECT * FROM Birth WHERE Year = '2000'",
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+
+
+def test_read_data():
+    """tests read_data"""
+    result = subprocess.run(
+        ["python", "main.py", "read_data"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+
+
+if __name__ == "__main__":
+    test_extract()
+    test_transform_load()
+    test_create_record()
+    test_read_data()
+    test_update_record()
+    test_delete_record()
+    test_general_query()
